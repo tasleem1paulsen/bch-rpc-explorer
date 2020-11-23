tasleem1paulsen/bch-rpc-explorer

commit 9c92bd3b450b911255d4c6a3bd44a2f1e5eb90a8

Use `getblockheader` instead of `getblock` in the TX details page
The template only used the block height, no need to fetch the full block.

routes/baseActionsRouter.js
@@ -936,10 +936,12 @@ router.get("/tx/:transactionId", function(req, res, next) {
 		}
 
 		promises.push(new Promise(function(resolve, reject) {
-			global.rpcClient.command('getblock', tx.blockhash, function(err3, result3, resHeaders3) {
-				res.locals.result.getblock = result3;
-
+			coreApi.getBlockHeader(tx.blockhash).then(function(blockHeader) {
+				res.locals.result.blockHeader = blockHeader;
 				resolve()
+			}).catch(function(err) {
+				res.locals.pageErrors.push(utils.logError("089d7fyg04334", err));
+				reject(err);
 			});
 		}));
 

views/transaction.pug
@@ -27,7 +27,7 @@ block content
 
 		- var totalInputValue = new Decimal(0);
 		if (result.getrawtransaction.vin[0].coinbase)
-			- totalInputValue = totalInputValue.plus(new Decimal(coinConfig.blockRewardFunction(result.getblock.height, activeBlockchain)));
+			- totalInputValue = totalInputValue.plus(new Decimal(coinConfig.blockRewardFunction(result.blockHeader.height, activeBlockchain)));
 		each txInput, txInputIndex in result.txInputs
 			if (txInput)
 				- var vout = txInput;
@@ -88,7 +88,7 @@ block content
 							div.row
 								div.summary-table-label Block
 								div.summary-table-content.text-monospace
-									a(href=("/block/" + result.getrawtransaction.blockhash)) ##{result.getblock.height.toLocaleString()}
+									a(href=("/block/" + result.getrawtransaction.blockhash)) ##{result.blockHeader.height.toLocaleString()}
 
 						if (isTxConfirmed)
 							div.row
@@ -152,7 +152,7 @@ block content
 									- var currencyValue = new Decimal(totalOutputValue).minus(totalInputValue);
 									include includes/value-display.pug
 
-							- var blockRewardMax = coinConfig.blockRewardFunction(result.getblock.height, activeBlockchain);
+							- var blockRewardMax = coinConfig.blockRewardFunction(result.blockHeader.height, activeBlockchain);
 							if (parseFloat(totalOutputValue) < parseFloat(blockRewardMax))
 								div.row
 									div.summary-table-label
@@ -262,8 +262,8 @@ block content
 						- var tx = result.getrawtransaction;
 						- var txInputs = result.txInputs;
 						- var blockHeight = -1;
-						if (result && result.getblock)
-							- blockHeight = result.getblock.height;
+						if (result && result.blockHeader)
+							- blockHeight = result.blockHeader.height;
 						include includes/transaction-io-details.pug
 
 				if (mempoolDetails)

commit 0253030e447747a9e715149a0ed999419d6ee8e8

Fix display of unconfirmed transactions
Commit 9c92bd3b450b911255d4c6a3bd44a2f1e5eb90a8 broke the TX page for
unconfirmed transactions. Error handling for `getblockheader` was added
without considering that there are situations where it is expected to
fail, like unconfirmed transactions.

routes/baseActionsRouter.js
@@ -940,15 +940,14 @@ router.get("/tx/:transactionId", function(req, res, next) {
 			}));
 		}
 
-		promises.push(new Promise(function(resolve, reject) {
-			coreApi.getBlockHeader(tx.blockhash).then(function(blockHeader) {
-				res.locals.result.blockHeader = blockHeader;
-				resolve()
-			}).catch(function(err) {
-				res.locals.pageErrors.push(utils.logError("089d7fyg04334", err));
-				reject(err);
-			});
-		}));
+		if (tx.blockhash !== undefined) {
+			promises.push(new Promise(function(resolve, reject) {
+				coreApi.getBlockHeader(tx.blockhash).then(function(blockHeader) {
+					res.locals.result.blockHeader = blockHeader;
+					resolve()
+				});
+			}));
+		}
 
 		Promise.all(promises).then(function() {
 			res.render("transaction");